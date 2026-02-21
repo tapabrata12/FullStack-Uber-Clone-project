caffafaa1cad619df06492b87e5f83a5b06cac0a
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2026-02-21 14:22:10 +0530

    feat: Initialize Express server with basic middleware and port listening.

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,6 @@
 require('dotenv').config();
-
 const app = require('./src/app');
-const express = require('express');
 const http = require('http');
-var cookieParser = require('cookie-parser')
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000
 const server = http.createServer(app);
--- Server/src/app.js
+++ Server/src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
-
 const app = express();
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 module.exports = app;
